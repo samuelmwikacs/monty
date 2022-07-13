9696a7948b9c3d64ec8b585206935ee1e8a52eea
mazi-kunle <olakunlemichael644@gmail.com>
2022-07-13 10:48:01 -0700

    implement pall

---
+++ print_stack.c
@@ -0,0 +1,22 @@
+#include "monty.h"
+/**
+ * print_stack- a function that prints the value in a stack.
+ * @head: head of the node.
+ *
+ * Return: no of nodes.
+*/
+size_t print_stack(const stack_t *head)
+{
+	stack_t *ptr;
+	size_t length;
+
+	length = 0;
+	ptr = head;
+	while (ptr != NULL)
+	{
+		printf("%d\n", ptr->n);
+		length++;
+		ptr = ptr->next;
+	}
+	return (length);
+}
