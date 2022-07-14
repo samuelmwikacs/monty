ac217ae5fab3e189788552c59b18dfc083a04cb9
mazi-kunle <102045752+mazi-kunle@users.noreply.github.com>
2022-07-14 04:31:41 +0200

    Create pop_stack.c

---
+++ pop_stack.c
@@ -0,0 +1,25 @@
+void pop(stack_t **head, unsigned int line_no)
+{
+	stack_t *ptr;
+  
+	ptr = *head;
+  if (*head == NULL)
+	{
+		fprintf(stderr, "<%u>: can't pop an empty stack\n", line_no);
+    exit(EXIT_FAILURE);
+	}
+	ptr = *head;
+  (*head)->next->prev = NULL;
+  *head = (*head)->next;
+  free(ptr);
+}
+
+/**
+ * wrapper_push- a wrapper function.
+ * @head: parameter.
+ * @line_no: parameter.
+*/
+void wrapper_pop(stack_t **head, unsigned int line_no)
+{
+	pop(head, line_no);
+}
